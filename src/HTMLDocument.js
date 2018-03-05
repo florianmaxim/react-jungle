493e849acf2768c86f90e3099c857997978ccd41
florianmaxim <hello@florianmaxim.com>
2018-03-05 21:32:08 +0100

    added HTMLDocument module

--- src/HTMLDocument.js
+++ src/HTMLDocument.js
@@ -6,8 +6,10 @@ export default class HTMLDocument {
         this.document  = `<!doctype html>`;
         this.document  = `<html>`;
         this.document += `<head>`;
+        this.document += `<meta charset="utf-8"/>`;
         this.document += `<title>${props.head.title}</title>`;
         this.document += `${props.head.styles}`;
+        this.document += `</head>`;        
         this.document += `<body>`;
         this.document += `<div id="root">${props.body.root}</div>`;
         this.document += `<script src="/bundle.js"></script>`;
